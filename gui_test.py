523fe57a122ac96ec2c8368487efde703ee25a37
Robison, Charles <charles.robison@cdk.com>
2016-07-13 17:01:47 -0700

    Added comments.

--- gui_test.py
+++ gui_test.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-root = Tk()
+root = Tk() # creates the container calling it root
 theLabel = Label(root, text="This is some very basic text for the container")
-theLabel.pack()
-root.mainloop()
+theLabel.pack() # puts the label into the container
+root.mainloop() # keeps the container open - continues the loop until broken by closing
